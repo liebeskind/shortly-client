aacadd7db0330ed5d8ec8380773a10601ac482cd
Daniel <daniel.liebeskind@gmail.com>
2013-10-25 18:56:35 -0700

    Adds search feature to index

--- public/client/app.js
+++ public/client/app.js
@@ -5,6 +5,10 @@ angular.module('shortly', [])
         templateUrl: 'templates/index.html',
         controller: 'homeCtrl'
       })
+      .when('/create', {
+        templateUrl: 'templates/create.html',
+        controller: 'createCtrl'
+      })
       .otherwise('/',{
         templateUrl:'templates/index.html'
       });
@@ -17,4 +21,18 @@ angular.module('shortly', [])
           console.log(data);
           $scope.links = data;
         });
-    });
+    })
+  .controller('createCtrl',
+    function($scope,$http){
+      $scope.submit = function() {
+        var obj = {url: this.url};
+        $http.post('/links', JSON.stringify(obj))
+        .success(function(data, status) {
+          console.log(data);
+          $scope.shortLink = data;
+          $scope.shortLink.shortURL = "/" + data.code;
+          $scope.shortLink.fullString = 'Heres your shitty link: http://localhost:4567/'+ data.code;
+        });
+      };
+    }
+  );
